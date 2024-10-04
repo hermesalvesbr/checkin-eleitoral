d3369d33d0b3d03602b147423877a969f7ebec19
hermesalvesbr <hermes.alves@araripina.com.br>
2024-10-04 19:41:18 -0300

    finalizando

--- pages/recebiveis/index.vue
+++ pages/recebiveis/index.vue
@@ -1,38 +1,60 @@
 <script setup lang="ts">
-// Estados, Municípios, Cargos e Vereadores
+import { computed, onMounted, ref, watch } from 'vue'
+
 const estados = ref([])
 const municipios = ref([])
-const vereadores = ref([]) // Lista de vereadores
+const vereadores = ref([])
+const loading = ref(false)
+const eleicaoData = ref(null)
 
-// Array fixo de cargos
 const cargos = ref([
   { codigo: 11, nome: 'Prefeito' },
   { codigo: 12, nome: 'Vice-prefeito' },
   { codigo: 13, nome: 'Vereador' },
 ])
 
-// Seleções dos selects
 const selectedUF = ref('')
 const selectedMunicipio = ref('')
 const selectedCargo = ref('')
 
-// Parâmetros fixos
 const anoEleicao = 2024
 const sqEleicao = 2045202024
-const codigoVereador = 13 // Código fixo para vereador
+const codigoVereador = 13
+const eleicaoUrl = computed(() => {
+  if (selectedUF.value && eleicaoData.value) {
+    const estadoSelecionado = estados.value.find(estado => estado.sigla === selectedUF.value)
+    return `https://divulgacandcontas.tse.jus.br/divulga/#/candidato/${estadoSelecionado?.regiao}/${selectedUF.value}/${eleicaoData.value}`
+  }
+  return null
+})
+async function fetchWithLocalStorage(key, url) {
+  const cachedData = localStorage.getItem(key)
+  if (cachedData) {
+    return JSON.parse(cachedData)
+  }
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    localStorage.setItem(key, JSON.stringify(data))
+    return data
+  }
+  catch (error) {
+    console.error('Erro ao buscar os dados:', error)
+    throw error
+  }
+}
 
-// Função para carregar os estados a partir do arquivo local
 async function carregarEstados() {
   try {
     const response = await fetch('/eleicao_atual.json')
     const data = await response.json()
-
-    // Verifica se os dados estão disponíveis
     if (data && data.ues) {
-      estados.value = data.ues.map((estado: { sigla: any, nome: any }) => ({
+      estados.value = data.ues.map(estado => ({
         sigla: estado.sigla,
         nome: estado.nome,
+        regiao: estado.regiao,
       }))
+      eleicaoData.value = data?.eleicao?.sq_ELEICAO
     }
     else {
       console.error('Dados de estados não encontrados')
@@ -47,9 +69,8 @@ async function carregarMunicipios(siglaUF: string) {
   try {
     const response = await fetch(`/municipios_${siglaUF}.json`)
     const data = await response.json()
-
     if (data && data.municipios) {
-      municipios.value = data.municipios.map((municipio: { nome: any, codigo: any }) => ({
+      municipios.value = data.municipios.map(municipio => ({
         nome: municipio.nome,
         codigo: municipio.codigo,
       }))
@@ -64,19 +85,75 @@ async function carregarMunicipios(siglaUF: string) {
   }
 }
 
-async function carregarVereadores(codigoMunicipio: any) {
+async function montarUrlPrestacaoContas(candidato) {
+  const sqEleicao = candidato.eleicao.id
+  const anoEleicao = candidato.eleicao.ano
+  const codigoMunicipio = candidato.ufCandidatura
+  const codigoCargo = candidato.cargo.codigo
+  const codigoPartido = String(candidato.numero).substring(0, 2)
+  const numeroCandidato = candidato.numero
+  const idCandidato = candidato.id
+
+  const url = `https://divulgacandcontas.tse.jus.br/divulga/rest/v1/prestador/consulta/${sqEleicao}/${anoEleicao}/${codigoMunicipio}/${codigoCargo}/${codigoPartido}/${numeroCandidato}/${idCandidato}`
+
+  const key = `prestacao_${idCandidato}`
+  const cachedData = localStorage.getItem(key)
+
+  if (cachedData) {
+    const parsedData = JSON.parse(cachedData)
+    if (parsedData.dadosConsolidados?.totalRecebido === 0) {
+      console.log('Total recebido é 0, ignorando cache e fazendo nova requisição para:', idCandidato)
+    }
+    else {
+      return parsedData
+    }
+  }
+
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    if (data.dadosConsolidados?.totalRecebido > 0) {
+      localStorage.setItem(key, JSON.stringify(data))
+    }
+    return data
+  }
+  catch (error) {
+    console.error('Erro ao buscar dados de prestação de contas:', error)
+    throw error
+  }
+}
+
+async function carregarVereadores(codigoMunicipio) {
   if (String(selectedCargo.value) === '13' && codigoMunicipio) {
+    loading.value = true
     try {
-      console.log(`Carregando vereadores para município: ${codigoMunicipio}`)
       const url = `https://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/listar/${anoEleicao}/${codigoMunicipio}/${sqEleicao}/${codigoVereador}/candidatos`
-      const response = await fetch(url)
-      const data = await response.json()
+      const key = `candidatos_${codigoMunicipio}_${anoEleicao}`
+      const data = await fetchWithLocalStorage(key, url)
+
       if (data && data.candidatos) {
-        vereadores.value = data.candidatos.map((candidato: { nomeUrna: any, numero: any, partido: { sigla: any } }) => ({
-          nome: candidato.nomeUrna, // Nome do candidato está em nomeUrna
-          numero: candidato.numero, // Número do candidato
-          partido: candidato.partido.sigla, // Sigla do partido
-        }))
+        vereadores.value = await Promise.all(
+          data.candidatos.map(async (candidato) => {
+            const urlPrestacaoContas = await montarUrlPrestacaoContas(candidato)
+            let totalRecebido = 0
+
+            try {
+              const responsePrestacao = await fetchWithLocalStorage(`prestacao_${candidato.id}`, urlPrestacaoContas)
+              totalRecebido = responsePrestacao.dadosConsolidados?.totalRecebido || 0
+            }
+            catch (error) {
+              console.error(`Erro ao carregar dados de prestação de contas para o candidato ${candidato.nomeUrna}:`, error)
+            }
+
+            return {
+              nome: candidato.nomeUrna,
+              numero: candidato.numero,
+              partido: candidato.partido.sigla,
+              idCandidato: candidato.id,
+              totalRecebido,
+            }
+          }),
+        )
       }
       else {
         console.error('Nenhum vereador encontrado para o município selecionado')
@@ -86,56 +163,54 @@ async function carregarVereadores(codigoMunicipio: any) {
     catch (error) {
       console.error(`Erro ao carregar vereadores para o município ${codigoMunicipio}:`, error)
     }
+    finally {
+      loading.value = false // Finaliza o carregamento
+    }
   }
 }
-// Função para agrupar os vereadores por partido
+
 const vereadoresAgrupadosPorPartido = computed(() => {
-  return vereadores.value.reduce((acumulador: any, vereador: { partido: string }) => {
-    // Se o partido já existe no acumulador, adiciona o vereador, senão, cria o array
+  return vereadores.value.reduce((acumulador: any, vereador) => {
     if (!acumulador[vereador.partido]) {
-      acumulador[vereador.partido] = []
+      acumulador[vereador.partido] = {
+        vereadores: [],
+        totalRecebido: 0,
+      }
     }
-    acumulador[vereador.partido].push(vereador)
+    acumulador[vereador.partido].vereadores.push(vereador)
+    acumulador[vereador.partido].totalRecebido += vereador.totalRecebido
     return acumulador
   }, {})
 })
 
-// Watcher para UF selecionado
 watch(selectedUF, (novoUF) => {
   if (novoUF) {
-    // Resetar município e cargo ao mudar o estado
     selectedMunicipio.value = ''
     selectedCargo.value = ''
     carregarMunicipios(novoUF)
   }
   else {
-    // Se o estado for deselecionado, também resetar município e cargo
     selectedMunicipio.value = ''
     selectedCargo.value = ''
     municipios.value = []
   }
 })
 
-// Watcher para Município selecionado
 watch(selectedMunicipio, (novoMunicipio) => {
   if (novoMunicipio) {
-    // Resetar cargo ao mudar o município
     selectedCargo.value = ''
   }
   else {
-    // Se o município for deselecionado, resetar o cargo
     selectedCargo.value = ''
   }
 })
 
 watch(selectedCargo, (novoCargo) => {
-  // Convertendo novoCargo para número antes de comparar
   if (Number(novoCargo) === 13 && selectedMunicipio.value) {
     carregarVereadores(selectedMunicipio.value)
   }
 })
 
-// Carregar os estados ao montar a página
 onMounted(() => {
   carregarEstados()
 })
@@ -143,7 +218,6 @@ onMounted(() => {
 
 <template>
   <v-container>
-    <!-- Select para UF -->
     <v-select
       id="uf"
       v-model="selectedUF"
@@ -154,7 +228,6 @@ onMounted(() => {
       outlined
     />
 
-    <!-- Select para Município -->
     <v-select
       id="municipio"
       v-model="selectedMunicipio"
@@ -166,7 +239,6 @@ onMounted(() => {
       :disabled="!selectedUF"
     />
 
-    <!-- Select para Cargo -->
     <v-select
       id="cargo"
       v-model="selectedCargo"
@@ -177,23 +249,44 @@ onMounted(() => {
       outlined
       :disabled="!selectedMunicipio"
     />
-    {{ selectedCargo }}
-    {{ Object.keys(vereadoresAgrupadosPorPartido).length }}
+
+    <v-alert
+      v-if="selectedCargo"
+      type="info"
+      color="green lighten-2"
+      elevation="2"
+      prominent
+      icon="mdi-information-outline"
+    >
+      <template #default>
+        <a :href="eleicaoUrl" target="_blank" class="text-decoration-none" style="color: yellow">
+          Confira essas informações no site oficial do TSE
+          / {{ selectedUF }}
+        </a>
+      </template>
+    </v-alert>
+    <!-- Indicador de carregamento -->
+    <v-progress-linear v-if="loading" indeterminate color="blue" />
+
     <v-card v-if="selectedCargo === 13 && Object.keys(vereadoresAgrupadosPorPartido).length > 0">
-      <v-card-title>Lista de Vereadores</v-card-title>
+      <v-card-title>Total líquido de Recursos Recebidos</v-card-title>
 
-      <!-- Iterar sobre os partidos -->
-      <v-list v-for="(vereadoresDoPartido, partido) in vereadoresAgrupadosPorPartido" :key="partido">
-        <v-list-subheader>{{ partido }}</v-list-subheader> <!-- Exibe o nome do partido -->
-        <v-divider /> <!-- Divisor para separar partidos -->
+      <v-list v-for="(dadosPartido, partido) in vereadoresAgrupadosPorPartido" :key="partido">
+        <v-list-subheader>
+          {{ partido }} - {{ dadosPartido.totalRecebido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }}
+        </v-list-subheader>
+        <v-divider />
 
-        <!-- Iterar sobre os vereadores de cada partido -->
-        <v-list-item v-for="(vereador, index) in vereadoresDoPartido" :key="index">
-          <v-list-item-title>{{ vereador.nome }}</v-list-item-title>
-          <v-list-item-subtitle>Número: {{ vereador.numero }} - Partido: {{ vereador.partido }}</v-list-item-subtitle>
+        <v-list-item v-for="(vereador, index) in dadosPartido.vereadores" :key="index">
+          <v-list-item-title>
+            {{ vereador.nome }} -
+            {{ vereador.totalRecebido > 0 ? vereador.totalRecebido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) : 'Indisponível' }}
+          </v-list-item-title>
+
+          <v-list-item-subtitle>Número: {{ vereador.numero }} - {{ vereador.partido }}</v-list-item-subtitle>
         </v-list-item>
 
-        <v-divider /> <!-- Divisor entre vereadores -->
+        <v-divider />
       </v-list>
     </v-card>
   </v-container>
